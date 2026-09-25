29e741918b3d99c25e84bdb2f54c6d84cb57aad2
markvaradi94 <mark.varadi03@gmail.com>
2026-09-25 12:19:29 +0300

    session 1 end

--- src/main/java/io/callisto/ticketing/TicketingApplication.java
+++ src/main/java/io/callisto/ticketing/TicketingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicketingApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(TicketingApplication.class, args);
 	}
 
